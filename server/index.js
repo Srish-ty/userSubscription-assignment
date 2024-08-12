2658b9aebc488de74ddd91f6df47d12d711e2894
Srishty M <srishtymangutte@gmail.com>
2024-08-12 21:46:48 +0530

    feat: delete and put methods in api admin endpint

--- server/index.js
+++ server/index.js
@@ -48,6 +48,64 @@ app.get("/asdf/admin", async (req, res) => {
   }
 });
 
+app.post("/asdf/admin", async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const existingUser = await collection.findOne({ email });
+
+    if (existingUser) {
+      res.status(400).send("Email already exists.");
+    } else {
+      await collection.insertOne({ name, email });
+      res.send("User added successfully!");
+    }
+  } catch (error) {
+    console.error("Error adding user:", error);
+    res.status(500).send("Error adding user");
+  }
+});
+
+app.put("/asdf/admin/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+
+  try {
+    const result = await collection.updateOne(
+      { _id: new MongoClient.ObjectId(id) },
+      { $set: { name, email } }
+    );
+
+    if (result.matchedCount === 0) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send("User updated successfully!");
+    }
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).send("Error updating user");
+  }
+});
+
+app.delete("/asdf/admin/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await collection.deleteOne({
+      _id: new MongoClient.ObjectId(id),
+    });
+
+    if (result.deletedCount === 0) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send("User deleted successfully!");
+    }
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).send("Error deleting user");
+  }
+});
+
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
 
